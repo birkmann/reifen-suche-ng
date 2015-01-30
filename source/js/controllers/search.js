558c2ff73f9dce6d78fee189f4e8e56ce4e23c39
Rafael Birkmann <rafkafu@yahoo.de>
2015-01-30 19:04:19 +0100

    Added ajax-calls

--- source/js/controllers/search.js
+++ source/js/controllers/search.js
@@ -4,21 +4,81 @@ myApp.controller('searchCtrl', function($scope, $http) {
 
 
 	$scope.vehicles = [
-		{ name:'Fahrzeugtyp', value:'' },
-		{ name:'PKW', value:'pkw' },
-		{ name:'LKW', value:'lkw' }
+		{ name:'Fahrzeugtyp', value:'all' },
+		{ name:'PKW', value:'PKW' },
+		{ name:'Motorrad', value:'Motorrad' },
+		{ name:'Offroad', value:'Offroad' },
+		{ name:'LKW', value:'LKW' },
+		{ name:'LLKW', value:'LLKW' }
 	];
 
 	$scope.seasons = [
-		{ name:'Jahreszeit', value:'' },
-		{ name:'Sommer', value:'sommer' },
-		{ name:'Winter', value:'winter' }
+		//{ name:'Alle', value:'all' },
+		{ name:'Sommer', value:'s' },
+		{ name:'Winter', value:'w' },
+		{ name:'Ganzjahres', value:'g' }
 	];
 
 	$scope.manufacturers = [
-		{ name:'Hersteller', value:'' },
+		{ name:'Hersteller', value:'all' },
+		{ name:'BF Goodrich', value:'BF Goodrich' },
 		{ name:'Bridgestone', value:'Bridgestone' },
-		{ name:'Michelin', value:'Michelin' }
+		{ name:'Continental', value:'Continental' },
+		{ name:'Dunlop', value:'Dunlop' },
+		{ name:'Falken', value:'Falken' },
+		{ name:'Firestone', value:'Firestone' },
+		{ name:'Goodyear', value:'Goodyear' },
+		{ name:'Hankook', value:'Hankook' },
+		{ name:'Michelin', value:'Michelin' },
+		{ name:'Pirelli', value:'Pirelli' },
+		{ name:'Vredestein', value:'Vredestein' },
+		{ name:'Yokohama', value:'Yokohama' }
+	];
+
+	$scope.speeds = [
+		{ name:'Geschwindigkeitsindex', value:'all' },
+		{ name:'B (bis 50 km/h)', value:'B' },
+		{ name:'E (bis 70 km/h)', value:'E' },
+		{ name:'F (bis 80 km/h)', value:'F' },
+		{ name:'G (bis 90 km/h)', value:'G' },
+		{ name:'H (bis 210 km/h)', value:'H' },
+		{ name:'J (bis 100 km/h)', value:'J' },
+		{ name:'K (bis 110 km/h)', value:'K' },
+		{ name:'L (bis 120 km/h)', value:'L' },
+		{ name:'M (bis 130 km/h)', value:'M' },
+		{ name:'N (bis 140 km/h)', value:'N' },
+		{ name:'P (bis 150 km/h)', value:'P' },
+		{ name:'Q (bis 160 km/h)', value:'Q' },
+		{ name:'R (bis 170 km/h)', value:'R' },
+		{ name:'S (bis 180 km/h)', value:'S' },
+		{ name:'T (bis 190 km/h)', value:'T' },
+		{ name:'V (bis 240 km/h)', value:'V' },
+		{ name:'W (bis 270 km/h)', value:'W' },
+		{ name:'Y (bis 300 km/h)', value:'Y' }
+	];
+
+	$scope.widths = [
+		{ name:'175', value:'175' },
+		{ name:'185', value:'185' },
+		{ name:'195', value:'195' },
+		{ name:'195', value:'195' },
+		{ name:'205', value:'205' },
+		{ name:'215', value:'215' },
+		{ name:'225', value:'225' }
+	];
+
+	$scope.heights = [
+		{ name:'45', value:'45' },
+		{ name:'50', value:'50' },
+		{ name:'55', value:'55' },
+		{ name:'60', value:'60' },
+		{ name:'65', value:'65' }
+	];
+
+	$scope.rims = [
+		{ name:'15', value:'15' },
+		{ name:'16', value:'16' },
+		{ name:'17', value:'17' }
 	];
 
 
@@ -27,11 +87,46 @@ myApp.controller('searchCtrl', function($scope, $http) {
 		$http({
 			method : 'POST',
 			url : 'process.php',
+			data: {
+				vehicle: $scope.vehicle,
+				season: $scope.season,
+				manufacturer: $scope.manufacturer,
+				speed: $scope.speed,
+				width: $scope.width,
+				height: $scope.height,
+				rim: $scope.rim
+			},
 			headers : {'Content-Type': 'application/x-www-form-urlencoded'}
 		}).success(function(data, status) {
 			$scope.results = data;
-        });
+		});
 
 	};
+
+	$scope.getHits = function() {
+
+		$http({
+			method : 'POST',
+			url : 'process.php',
+			data: {
+				vehicle: $scope.vehicle,
+				season: $scope.season,
+				manufacturer: $scope.manufacturer,
+				speed: $scope.speed,
+				width: $scope.width,
+				height: $scope.height,
+				rim: $scope.rim
+			},
+			headers : {'Content-Type': 'application/x-www-form-urlencoded'}
+		}).success(function(data, status) {
+			$scope.hits = Object.keys(data).length;
+			return $scope.hits;
+		});
+
+	};
+
+	$scope.searchChanged = function() {
+		$scope.getHits();
+	}
 
 });
--- source/php/process.php
+++ source/php/process.php
@@ -1,11 +1,50 @@
 <?php
-	
-	include "dbconnect.php";
+
+    include "dbconnect.php";
 
     $db = mysql_select_db($database);
     $db = mysql_connect($host, $username, $password);
 
-    $result = mysql_query("select * from reifen LIMIT 100 OFFSET 1", $db);
+    $postdata = file_get_contents("php://input");
+    $request = json_decode($postdata);
+
+    @$vehicle = $request->vehicle->{'value'};
+    @$manufacturer = $request->manufacturer->{'value'};
+    @$speed = $request->speed->{'value'};
+    @$width = $request->width->{'value'};
+    @$height = $request->height->{'value'};
+    @$rim = $request->rim->{'value'};
+
+    @$season = $request->season->{'value'};
+
+    if ($season == "w") {
+        $seasontext = "Winterreifen";
+    };
+
+    if ($season == "s") {
+        $seasontext = "Sommerreifen";
+    };
+
+    if ($season == "g") {
+        $seasontext = "Ganzjahresreifen";
+    };
+
+    if ($manufacturer!="all" && $speed!="all") {
+        $query = "SELECT * FROM reifen WHERE `COL 15`='$vehicle' AND `COL 14`='$season' AND `COL 6`='$manufacturer' AND `COL 13`='$speed' AND `COL 9`='$width' AND `COL 10`='$height' AND `COL 11`='$rim' ORDER BY `COL 4` * 1 ASC";
+    }
+    else if($manufacturer!="all" && $speed=="all" ) {
+        $query = "SELECT * FROM reifen WHERE `COL 15`='$vehicle' AND `COL 14`='$season' AND `COL 6`='$manufacturer' AND `COL 9`='$width' AND `COL 10`='$height' AND `COL 11`='$rim' ORDER BY `COL 4` * 1 ASC";
+    }
+    else if($manufacturer=="all" && $speed!="all" ) {
+        $query = "SELECT * FROM reifen WHERE `COL 15`='$vehicle' AND `COL 14`='$season' AND `COL 13`='$speed' AND `COL 9`='$width' AND `COL 10`='$height' AND `COL 11`='$rim' ORDER BY `COL 4` * 1 ASC";
+    }
+    else if($manufacturer=="all" && $speed=="all" ) {
+        $query = "SELECT * FROM reifen WHERE `COL 15`='$vehicle' AND `COL 14`='$season' AND `COL 9`='$width' AND `COL 10`='$height' AND `COL 11`='$rim' ORDER BY `COL 4` * 1 ASC";
+    }
+
+    //$query = "SELECT * FROM reifen LIMIT 100 OFFSET 1";
+
+    $result = mysql_query($query, $db);
     
     $json_response = array();
 
@@ -19,7 +58,7 @@
         $row_array['rim']               = htmlentities($row['COL 11']);
         $row_array['loadindex']         = htmlentities($row['COL 11']);
         $row_array['speedindex']        = htmlentities($row['COL 13']);
-        $row_array['season']            = htmlentities($row['COL 14']);
+        $row_array['season']            = $seasontext;
         $row_array['vehicle']           = htmlentities($row['COL 15']);
         $row_array['fuel']              = htmlentities($row['COL 17']);
         $row_array['grip']              = htmlentities($row['COL 18']);
